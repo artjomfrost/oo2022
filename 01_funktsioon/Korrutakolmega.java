2e7b0816b19efdd022916ed1250661273352ccf0
Artjom Morozov <artjomfrost@users.noreply.github.com>
2022-02-13 23:53:50 +0200

    Esimene loeng

---
+++ 01_funktsioon/Korrutakolmega.java
@@ -0,0 +1,9 @@
+public class Korrutakolmega {
+    public static void main(String[] arg){
+        int arv1=Integer.parseInt(arg[0]);
+        System.out.println(arv1*3);
+        for(int i=0; i<3; i++) {
+            System.out.println("Tere");
+        }
+    }
+}
---
+++ 01_funktsioon/Tervitus2.java
@@ -0,0 +1,11 @@
+public class Tervitus2 {
+    public static void main(String[] arg){
+        if(arg.length<=1){
+            System.out.println("Tere, "+arg[0]);
+        }
+        if(arg.length>1){
+            System.out.println("Tere, " + arg[0] + " ja " + arg[1]);
+        }
+        }
+
+}
